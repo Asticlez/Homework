23576169828d255c7cbe3ae190bfb59852cd31f8
Asticlez <s6630613033@phuket.psu.ac.th>
2024-10-18 12:19:53 +0700

    now

--- asticlez/next.config.mjs
+++ asticlez/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['avatars.githubusercontent.com', 'media1.tenor.com'], // Add 'media1.tenor.com'
+    },
+  };
+  
+  export default nextConfig;
